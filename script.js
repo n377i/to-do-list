9c3ea1625e40316ce55a94416305a234d1be7f17
n377i <annettewoehler@gmx.de>
2023-07-12 17:02:39 +0200

    + display notice if input-field is empty when add-button is pushed

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 
 const inputAddTask = document.querySelector("#input-field");
 const btnAddTask = document.querySelector("#add-btn");
+const notice = document.querySelector("#notice");
 const toDoList = document.querySelector("#list");
 
 btnAddTask.addEventListener("click", () => {
   if(inputAddTask.value) {
+    notice.innerText = null;
+    inputAddTask.style.backgroundColor = "var(--grey)";
     console.log(inputAddTask.value);
+  } else {
+    inputAddTask.style.backgroundColor = "var(--light-red)";
+    notice.innerText = "Please enter task first.";
   }
 })
