962ed4db4562ee277eb4e600ed004698eedefaa3
n377i <annettewoehler@gmx.de>
2023-07-13 20:08:53 +0200

    update scripst.js
    + add function to create tasks
    + save tasks to local storage
    * update to-do form event
    * change id and variable names

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
-"use strict";
+'use strict';
 
-const inputAddTask = document.querySelector("#input-field");
-const btnAddTask = document.querySelector("#add-btn");
-const notice = document.querySelector("#notice");
-const toDoList = document.querySelector("#list");
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = document.querySelector('#todo-form input');
+const btnAddTask = document.querySelector('#add-btn');
+const notice = document.querySelector('#notice');
+const toDoList = document.querySelector('#todo-list');
 
-btnAddTask.addEventListener("click", () => {
-  if(inputAddTask.value) {
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+toDoForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  if(toDoInput.value) {
     notice.innerText = null;
-    inputAddTask.style.backgroundColor = "var(--grey)";
-    console.log(inputAddTask.value);
+    toDoInput.style.backgroundColor = 'var(--grey)';
+
+    const task = {
+      id: new Date().getTime(),
+      name: toDoInput.value,
+      isCompleted: false
+    }
+
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    createTask(task);
+
+    toDoForm.reset();
+    toDoInput.focus();
+
   } else {
-    inputAddTask.style.backgroundColor = "var(--light-red)";
-    notice.innerText = "Please enter task first.";
+    toDoInput.style.backgroundColor = 'var(--light-red)';
+    notice.innerText = 'Please enter task first.';
+    toDoInput.focus();
   }
 })
+
+const createTask = task => {
+  const listItem = document.createElement('li');
+  listItem.setAttribute('id', task.id);
+
+  if(task.isCompleted) {
+    listItem.classList.add('completed');
+    toDoInput.style.backgroundColor = 'var(--light-green)';
+  }
+
+  const listItemMarkup = `
+    <div>
+      <input type="checkbox" id="${task.id}" class="checkbox" ${task.isCompleted ? "checked" : ""}>
+        <label ${task.isCompleted ? "contenteditable" : ""}>${task.name}</label>
+    </div>
+    <button title="Remove task" class="remove-btn">x</button>
+  `;
+  listItem.innerHTML = listItemMarkup;
+  toDoList.appendChild(listItem);
+}
+
+if(localStorage.getItem('tasks')) {
+  tasks.map(task => {
+    createTask(task);
+  })
+}
