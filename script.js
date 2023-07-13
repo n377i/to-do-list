49fa8f1cd575da0355f2233fce0f87dcefe9a1f7
n377i <annettewoehler@gmx.de>
2023-07-13 22:31:07 +0200

    +add functions for removing and counting completed and total tasks

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 const toDoForm = document.querySelector('#todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const btnAddTask = document.querySelector('#add-btn');
+const btnRemoveTask = document.querySelector('#remove-btn');
 const notice = document.querySelector('#notice');
 const toDoList = document.querySelector('#todo-list');
+const completedTasks = document.querySelector('#completed-tasks');
+const totalTasks = document.querySelector('#total-tasks');
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
@@ -36,6 +39,14 @@ toDoForm.addEventListener('submit', evt => {
   }
 })
 
+toDoList.addEventListener("click", evt => {
+  if(evt.target.classList.contains('remove-btn')) {
+    const taskId = evt.target.closest('li').id;
+
+    removeTask(taskId);
+  }
+})
+
 const createTask = task => {
   const listItem = document.createElement('li');
   listItem.setAttribute('id', task.id);
@@ -47,13 +58,34 @@ const createTask = task => {
 
   const listItemMarkup = `
     <div>
-      <input type="checkbox" id="${task.id}" class="checkbox" ${task.isCompleted ? "checked" : ""}>
-        <label ${task.isCompleted ? "contenteditable" : ""}>${task.name}</label>
+      <input type="checkbox" id="${task.id}" class="checkbox" ${task.isCompleted ? 'checked' : ''}>
+        <label ${!task.isCompleted ? 'contenteditable' : ''}>${task.name}</label>
     </div>
     <button title="Remove task" class="remove-btn">x</button>
   `;
   listItem.innerHTML = listItemMarkup;
   toDoList.appendChild(listItem);
+
+  countTasks();
+}
+
+const removeTask = (taskId) => {
+  tasks = tasks.filter(task =>
+    task.id != parseInt(taskId)
+  );
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  document.getElementById(taskId).remove();
+  countTasks();
+}
+
+const countTasks = () => {
+  const completedTasksArr = tasks.filter(task => 
+    task.isCompleted === true
+  );
+    completedTasks.innerText = completedTasksArr.length;
+    totalTasks.innerText = tasks.length;
 }
 
 if(localStorage.getItem('tasks')) {
