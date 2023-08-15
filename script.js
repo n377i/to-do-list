f75c4489de8b8fad6c4f963e693d339575236d30
n377i <annettewoehler@gmx.de>
2023-08-15 22:43:58 +0200

    Update script.js
    * rework entire code
    + add functions for edit menu options

--- script.js
+++ script.js
@@ -7,151 +7,145 @@ const completedTasks = document.querySelector('#completed-tasks');
 const totalTasks = document.querySelector('#total-tasks');
 const overlay = document.querySelector('#overlay');
 const editMenu = document.querySelector('#edit-menu');
-const removeLink = document.querySelector('#remove');
-const cancelLink = document.querySelector('#cancel');
 
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem('tasks')) || []; // get tasks from local storage or pass an empty array
+let editingMode = false;
+let editingTaskId = null;
 
 toDoForm.addEventListener('submit', evt => {
   evt.preventDefault();
+  taskInput.style.backgroundColor = 'var(--white)';
+  taskInput.style.outline = 'none';
 
-  if(taskInput.value) {
-    taskInput.style.backgroundColor = 'var(--white)';
-    taskInput.style.outline = 'none';
-
-    const task = {
-      id: new Date().getTime(),
-      name: taskInput.value,
-      isCompleted: false
+  if (taskInput.value.trim()) {
+    if (editingMode && editingTaskId !== null) {
+      // update task name and reset mode
+      tasks[editingTaskId].name = taskInput.value;
+      editingMode = false;
+      editingTaskId = null;
+    } else {
+      const taskInfo = {
+        name: taskInput.value,
+        status: 'pending'
+      }
+      tasks.push(taskInfo);
     }
-
-    tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    createTask(task);
-
+    createTask();
     toDoForm.reset();
     taskInput.focus();
-
   } else {
     taskInput.style.outline = '1px solid var(--red)';
-    taskInput.style.backgroundColor = 'var(--light-red)'
+    taskInput.style.backgroundColor = 'var(--light-red)';
     taskInput.focus();
   }
-})
+});
 
-toDoList.addEventListener('input', evt => {
-  const taskId = evt.target.closest('li').id;
-
-  updateTask(taskId, evt.target);
-})
-
-toDoList.addEventListener('keydown', evt => {
-  if(evt.keyCode === 13) {
-    evt.preventDefault();
-    evt.target.blur();
+const createTask = () => {
+  let li = '';
+  if (tasks) {
+    tasks.forEach((task, id) => {
+      const checked = task.status === 'completed' ? 'checked' : '';
+      li += `
+          <li class="${checked}">
+              <label for="${id}">
+                  <input type="checkbox" id="${id}" class="checkbox" ${checked}>
+                  <p>${task.name}</p>
+              </label>
+              <img class="edit-btn" data-task-id="${id}" src="img/icon_edit-menu.svg" alt="edit menu">
+          </li>
+          `;
+    });
   }
-})
+  toDoList.innerHTML = li;
 
-toDoList.addEventListener('click', evt => {
-if(evt.target.classList.contains('edit-btn')) {
-  const taskId = evt.target.closest('li').id;
+  countTasks();
+}
 
-  removeLink.dataset.taskId = taskId;
-  cancelLink.dataset.taskId = taskId;
+const updateStatus = (id) => {
+  tasks[id].status = tasks[id].status === 'completed' ? 'pending' : 'completed';
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 
-  editMenu.classList.toggle('open');
+  // update class of list element
+  const liElement = document.querySelector(`li:nth-child(${id + 1})`);
+  if (tasks[id].status === 'completed') {
+    liElement.classList.add('checked');
+  } else {
+    liElement.classList.remove('checked');
+  }
+
+  countTasks();
+}
+
+// event delegation for checkboxes
+toDoList.addEventListener('change', (evt) => {
+  if (evt.target.matches('.checkbox')) {
+    const taskId = parseInt(evt.target.id);
+    updateStatus(taskId);
+  }
+});
+
+toDoList.addEventListener('click', (evt) => {
+  if (evt.target.matches('.edit-btn')) {
+    const taskId = evt.target.getAttribute('data-task-id');
+    showEditMenu(taskId);
+  }
+});
+
+const showEditMenu = (taskId) => {
+  editMenu.setAttribute('data-current-task-id', taskId); // save current task ID
+  editMenu.classList.toggle('open'); // show menu
   overlay.classList.toggle('open');
 }
-});
 
-editMenu.addEventListener('click', evt => {
-  const taskId = evt.target.dataset.taskId;
-  if (evt.target === removeLink) {
-      editMenu.classList.remove('open');
-      overlay.classList.remove('open');
-      removeTask(taskId);
-  } else if (evt.target === cancelLink) {
-      editMenu.classList.remove('open');
-      overlay.classList.remove('open');
+editMenu.addEventListener('click', (evt) => {
+  const taskId = editMenu.getAttribute('data-current-task-id');
+
+  if (evt.target.closest('#edit')) {
+    // set edit mode and save task ID
+    editingMode = true;
+    editingTaskId = parseInt(taskId);
+
+    // show task name in input field
+    taskInput.value = tasks[taskId].name;
+    taskInput.style.outline = 'none';
+    taskInput.style.backgroundColor = 'var(--light-green)';
+    taskInput.focus();
+    editMenu.classList.remove('open'); // close menu
+    overlay.classList.remove('open');
+  } else if (evt.target.closest('#delete')) {
+    // delete-logic
+    tasks.splice(taskId, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    createTask();
+    editMenu.classList.remove('open'); // close menu
+    overlay.classList.remove('open');
+    // close menu when "Cancel" is clicked
+  } else if (evt.target.closest('#cancel')) { 
+    editMenu.classList.remove('open');
+    overlay.classList.remove('open');
   }
 });
 
-const createTask = task => {
-  const listItem = document.createElement('li');
-  listItem.setAttribute('id', task.id);
-
-  if(task.isCompleted) {
-    listItem.classList.add('completed');
+// close menu when clicked outside of it
+document.addEventListener('click', (evt) => {
+  if (!editMenu.contains(evt.target) && !evt.target.matches('.edit-btn')) {
+    editMenu.classList.remove('open');
+    overlay.classList.remove('open');
   }
-
-  const listItemMarkup = `
-    <div>
-      <input type="checkbox" id="${task.id}" class="checkbox" ${task.isCompleted ? 'checked' : ''}>
-        <label ${!task.isCompleted ? 'contenteditable' : ''}>${task.name}</label>
-    </div>
-    <img class="edit-btn" src="img/icon_edit-menu.svg" alt="edit menu">
-  `;
-  listItem.innerHTML = listItemMarkup;
-  toDoList.appendChild(listItem);
-
-  countTasks();
-}
-
-const updateTask = (taskId, el) => {
-  const task = tasks.find(task =>
-    task.id === parseInt(taskId)
-  );
-
-  if(el.hasAttribute('contenteditable')) {
-    task.name = el.innerText;
-  } else {
-    const span = el.nextElementSibling;
-    const parent = el.closest('li');
-
-    task.isCompleted = !task.isCompleted;
-
-    if(task.isCompleted) {
-      span.removeAttribute('contenteditable');
-      parent.classList.add('completed');
-    } else {
-      span.setAttribute('contenteditable', 'true');
-      parent.classList.remove('completed');
-    }
-  }
-
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-
-  countTasks();
-}
-
-const removeTask = taskId => {
-  tasks = tasks.filter(task =>
-    task.id !== parseInt(taskId)
-  );
-
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-
-  if(taskId !== null) {
-    document.getElementById(taskId).remove();
-    countTasks();
-  }
-}
+});
 
 const countTasks = () => {
-  const completedTasksArr = tasks.filter(task => 
-    task.isCompleted === true
+  const completedTasksArr = tasks.filter(task =>
+    task.status === 'completed'
   );
-    completedTasks.innerText = completedTasksArr.length;
-    totalTasks.innerText = tasks.length;
+  completedTasks.innerText = completedTasksArr.length;
+  totalTasks.innerText = tasks.length;
 
-    const progressPercent = (completedTasksArr.length / tasks.length) * 100;
-    const progressBar = document.querySelector('.progress');
-    progressBar.style.width = `${progressPercent}%`;
+  const progressPercent = (completedTasksArr.length / tasks.length) * 100;
+  const progressBar = document.querySelector('.progress');
+  progressBar.style.width = `${progressPercent}%`;
 }
 
-if(localStorage.getItem('tasks')) {
-  tasks.map(task => {
-    createTask(task);
-  })
-}
+// display tasks when the page loads
+createTask();
